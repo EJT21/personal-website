f255bec8cabb968954a1f9a08eab2477efebc95e
Erick <etommy21@gmail.com>
2026-08-31 20:00:29 -0400

    Add About section scaffold
    Create the initial About component for the personal website. This adds a new section shell with layout scaffolding for future content and styling.

---
+++ personal-website/src/components/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <section id="about" className="min-h-screen px-10 py-20">
+            <div className="max-w-4xl mx-auto">
+
+            </div>
+        </section>
+    );
+}
